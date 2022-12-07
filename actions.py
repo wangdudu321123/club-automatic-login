779759f68a28dba5bb51d6b5fd2c95f2629344d1
thewon86 <zhongyong0104@sina.com>
2022-12-07 17:19:30 +0800

    Update actions.py

--- actions.py
+++ actions.py
@@ -39,6 +39,9 @@ def main():
     execute_command("sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1")
     execute_command("sudo dpkg -i google-chrome-stable_current_amd64.deb > /dev/null 2>&1; apt-get -fy install > /dev/null 2>&1")
     day_num = login_in_club(username, password)
+    if day_num == None:
+        sys.exit(1)
+
     try:
         with open("sign_in_days.txt", "w") as f:
             f.write(day_num)
