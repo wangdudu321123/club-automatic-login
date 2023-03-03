eb868f3ee13c4a4804f08d7f5185c568a3e19803
thewon86 <zhongyong0104@sina.com>
2023-03-03 10:32:44 +0800

    Update actions_rtt.py

--- actions_rtt.py
+++ actions_rtt.py
@@ -39,8 +39,6 @@ def main():
 #    execute_command("sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /dev/null 2>&1")
 #    execute_command("sudo dpkg -i google-chrome-stable_current_amd64.deb > /dev/null 2>&1; apt-get -fy install > /dev/null 2>&1")
 
-    delay = random.randint(1, 1200)
-    time.sleep(delay)
     day_num = login_in_club(username, password)
     if day_num == None:
         sys.exit(1)
